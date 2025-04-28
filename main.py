a46b250ae8dc72e51654e1b799d778a8730223a8
Youssef <youssefmail1456@gmail.com>
2025-04-28 04:42:58 +0100

    Fix error and some improvements

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from game_logic import *
+from game_logic import Game, State, RandomPlayer,  HumanPlayerByCommandLine, HumanPlayerByGUI
 #from game_ai import *
 
 # # To start game of Two human player
 # g = Game([HumanPlayerByCommandLine("Alex"), HumanPlayerByCommandLine("Ali")])
 # g.start_game()
 
-# To start game of random player vs human player
-g = Game([HumanPlayerByCommandLine("Ali"), RandomPlayer("random")])
-g.start_game()
+# Test game_logic has no errors
+for i in range(10):
+    g = Game([RandomPlayer("random1"), RandomPlayer("random2")])
+    g.start_game()
+
+# # To start game of random player vs human player
+# g = Game([HumanPlayerByCommandLine("Ali"), RandomPlayer("random")])
+# g.start_game()
+
+# # To start game of random player vs ai player
+# g = Game([HumanPlayerByCommandLine("Ali"), AiPlayer("random")])
+# g.start_game()
 
 # # To start game of random player
 # g = Game([RandomPlayer("Random1"), RandomPlayer("Random2")])
 # g.start_game()
 
-# games recored at "games.csv" to can retrive them
+# games recored at "games_history.json" to can retrive them
