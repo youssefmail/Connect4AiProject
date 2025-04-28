178ef08aa18d088e04ae9b06bf05913525bc1bd6
youssefmail <youssefmail1456@gmail.com>
2025-04-28 02:01:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from game_logic import Game, State, RandomPlayer,  HumanPlayerByCommandLine, HumanPlayerByGUI
-#from game_ai import *
+from game_ai import *
 
-# # To start game of Two human player
+# # To start game of human players
 # g = Game([HumanPlayerByCommandLine("Alex"), HumanPlayerByCommandLine("Ali")])
 # g.start_game()
 
-# Test game_logic has no errors
-for i in range(10):
-    g = Game([RandomPlayer("random1"), RandomPlayer("random2")])
-    g.start_game()
+# # Test game_logic has no errors
+# for i in range(1000):
+#     g = Game([RandomPlayer("random1"), RandomPlayer("random2")])
+#     g.start_game()
 
-# # To start game of random player vs human player
+# # To start game of random players
+# g = Game([RandomPlayer("Random1"), RandomPlayer("Random2")])
+# g.start_game()
+
+# # To start game of human player vs random player
 # g = Game([HumanPlayerByCommandLine("Ali"), RandomPlayer("random")])
 # g.start_game()
 
-# # To start game of random player vs ai player
-# g = Game([HumanPlayerByCommandLine("Ali"), AiPlayer("random")])
-# g.start_game()
-
-# # To start game of random player
-# g = Game([RandomPlayer("Random1"), RandomPlayer("Random2")])
-# g.start_game()
+# To start game of human player vs ai player
+g = Game([HumanPlayerByCommandLine("Ali"), AiPlayer("Ai player")])
+g.start_game()
 
 # games recored at "games_history.json" to can retrive them
