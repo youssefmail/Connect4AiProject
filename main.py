7b0060ac7cc0d1620e27008eba14fe718960e415
Youssef <youssefmail1456@gmail.com>
2025-05-01 09:34:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from game_logic import Game, State, RandomPlayer,  HumanPlayerByCommandLine, HumanPlayerByGUI
-from game_ai import *
+from game_ai import AiPlayer
 
 # # To start game of human players
 # g = Game([HumanPlayerByCommandLine("Alex"), HumanPlayerByCommandLine("Ali")])
 # g.start_game()
-
-# # Test game_logic has no errors
-# for i in range(1000):
-#     g = Game([RandomPlayer("random1"), RandomPlayer("random2")])
-#     g.start_game()
 
 # # To start game of random players
 # g = Game([RandomPlayer("Random1"), RandomPlayer("Random2")])
@@ -19,11 +14,38 @@ from game_ai import *
 # g.start_game()
 
 # To start game of human player vs ai player
-g = Game([HumanPlayerByCommandLine("Human"), AiPlayer(2, "Ai player")])
+g = Game([HumanPlayerByCommandLine("Human"), AiPlayer()])
 g.start_game()
 
+# # To start game of human player vs ai player (WHY AI NOT CHOOSE ACTION 7 !! WHY CHOOSED ACTION 1 !!)
+# g = Game([HumanPlayerByCommandLine("Human"), AiPlayer()],init_state=State(actions_list=[
+#         2,
+#         3,
+#         2,
+#         2,
+#         3,
+#         0,
+#         2,
+#         3,
+#         4,
+#         3,
+#         4,
+#         3,
+#         3,
+#         4,
+#         5,
+#         5,
+#         5,
+#         0,
+#         0,
+#         # 0,
+#         # 2,
+#         # 6
+#     ]))
+# g.start_game()
+
 # # To start game of ai player vs ai player
-# g = Game([AiPlayer(1, "Ai player 1"), AiPlayer(2, "Ai player 2",level=4)])
+# g = Game([AiPlayer(), AiPlayer(level=4)])
 # g.start_game()
 
 # games recored at "games_history.json" to can retrive them
